8bfe832bcb7026b5899a57befea9f236579bf9b2
Gabriel <andreign95@gmail.com>
2018-11-02 12:13:54 +0200

    use openstack client instead of neutron client fix (#20)

--- templates/devstack/local.sh
+++ templates/devstack/local.sh
@@ -2,30 +2,30 @@
 set -e
 
 echo "Before updating nova flavors:"
-nova flavor-list
+openstack flavor list
 
-nova flavor-delete 42 || echo "Flavor 42 not found"
-nova flavor-delete 84 || echo "Flavor 42 not found"
-nova flavor-delete 451 || echo "Flavor 42 not found"
+openstack flavor delete 42 || echo "Flavor 42 not found"
+openstack flavor delete 84 || echo "Flavor 42 not found"
+openstack flavor delete 451 || echo "Flavor 42 not found"
 
-nova flavor-create m1.nano 42 256 1 1
-nova flavor-create m1.micro 84 256 2 1
-nova flavor-create m1.heat 451 512 1 1
-nova flavor-create m1.heat2 452 1024 1 1
+openstack flavor create m1.nano 42 256 1 1
+openstack flavor create m1.micro 84 256 2 1
+openstack flavor create m1.heat 451 512 1 1
+openstack flavor create m1.heat2 452 1024 1 1
 
 echo "After updating nova flavors:"
-nova flavor-list
+openstack flavor list
 
 # Add DNS config to the private network
-subnet_id=`neutron net-show private | grep subnets | awk '{print $4}'`
-neutron subnet-update $subnet_id --dns_nameservers list=true 8.8.8.8 8.8.4.4
+subnet_id=`openstack network show private | grep subnets | awk '{print $4}'`
+openstack subnet set $subnet_id --dns-nameserver 8.8.8.8 --dns-nameserver 8.8.4.4
 
 echo "Neutron networks:"
-neutron net-list
-for net in `neutron net-list | grep -v '\-\-' | grep -v "subnets" | awk {'print $2'}`; do neutron net-show $net; done
+openstack network list
+for net in `openstack network list| grep -v '\-\-' |grep -v "Subnets" | awk {'print $2'}`; do openstack network show $net; done
 echo "Neutron subnetworks:"
-neutron subnet-list
-for subnet in `neutron subnet-list | grep start | awk {'print $2'}`; do neutron subnet-show $subnet; done
+openstack subnet list
+for subnet in `openstack subnet list | grep -v '\-\-' |grep -v "ID" |awk {'print $2'}`; do openstack subnet show $subnet; done
 
 {% if localsh_append is defined %}
 {{ localsh_append }}
